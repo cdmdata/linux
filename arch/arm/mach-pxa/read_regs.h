24c4d5c668c4b5310246133539e4e7e2b0e2cd54
Valliappan Annamalai <valli@valli-laptop.(none)>
2009-01-18 00:49:43 -0700

    added functions to read values from pxa registers

---
+++ arch/arm/mach-pxa/read_regs.h
@@ -0,0 +1,22 @@
+#ifndef __PXA_READ_REGS_H__
+#define __PXA_READ_REGS_H__
+
+/*
+ * arch/arm/mach-pxa/read_regs.h
+ *
+ * Author: Valliappan (Valli) Annamalai, Boundary Devices
+ *
+ * This header file defines the function to read values
+ * stored in PXA registers. This function would be called
+ * from the board specific init method.
+ *
+ */
+
+#include <linux/platform_device.h>
+#include <mach/pxa-regs.h>
+#include <mach/pxa2xx-regs.h>
+#include <mach/pxafb.h>
+
+void pxa_mode_from_registers(struct platform_device *dev);
+
+#endif
