4897ce17c938264654ffb95b65557c1293557778
Eric Nelson <ericn@ericsony.(none)>
2009-06-04 17:38:33 -0700

    Set reset pin to ALT function 2

--- sound/arm/pxa2xx-ac97-lib.c
+++ sound/arm/pxa2xx-ac97-lib.c
@@ -65,8 +65,8 @@ static void set_resetgpio_mode(int resetgpio_action)
 		switch (resetgpio_action) {
 		case RESETGPIO_NORMAL_ALTFUNC:
 			if (reset_gpio == 113)
-				mode = 113 | GPIO_OUT | GPIO_DFLT_LOW;
-			if (reset_gpio == 95)
+				mode = 113 | GPIO_ALT_FN_2_OUT;
+			else if (reset_gpio == 95)
 				mode = 95 | GPIO_ALT_FN_1_OUT;
 			break;
 		case RESETGPIO_FORCE_LOW:
