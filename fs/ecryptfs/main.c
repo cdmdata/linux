b1203e5457d12e085da08ef5953fd41a7aa54425
Eric Nelson <ericn@ericsony.(none)>
2009-06-29 14:15:35 -0700

    remove unionfs

--- fs/ecryptfs/main.c
+++ fs/ecryptfs/main.c
@@ -190,10 +190,6 @@ int ecryptfs_interpose(struct dentry *lower_dentry, struct dentry *dentry,
 		init_special_inode(inode, lower_inode->i_mode,
 				   lower_inode->i_rdev);
 	dentry->d_op = &ecryptfs_dops;
-	if (flags & ECRYPTFS_INTERPOSE_FLAG_D_ADD)
-		d_add(dentry, inode);
-	else
-		d_instantiate(dentry, inode);
 	fsstack_copy_attr_all(inode, lower_inode, NULL);
 	/* This size will be overwritten for real files w/ headers and
 	 * other metadata */
