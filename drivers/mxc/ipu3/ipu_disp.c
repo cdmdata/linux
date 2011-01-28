c8732e27f5b5ba2ba263e49f044682f299a49d2a
Eric Nelson <eric.nelson@boundarydevices.com>
2011-01-28 12:20:19 -0700

    pll3 can't be the parent of the pixel clock

--- drivers/mxc/ipu3/ipu_disp.c
+++ drivers/mxc/ipu3/ipu_disp.c
@@ -1097,7 +1097,8 @@ int32_t ipu_init_sync_panel(int disp, uint32_t pixel_clk,
 		 */
 		if (!sig.int_clk &&
 			((rounded_pixel_clk >= pixel_clk + pixel_clk/16) ||
-			(rounded_pixel_clk <= pixel_clk - pixel_clk/16))) {
+			(rounded_pixel_clk <= pixel_clk - pixel_clk/16)) &&
+			(clk_get(NULL, "pll3") != di_parent)) {
 			dev_dbg(g_ipu_dev, "try ipu ext di clk\n");
 			rounded_pixel_clk = pixel_clk * 2;
 			while (rounded_pixel_clk < 150000000)
