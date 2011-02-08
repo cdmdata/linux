2a7032b2626c5ea1fee92af1d17da5581b101655
Eric Nelson <eric.nelson@boundarydevices.com>
2011-02-08 15:42:06 -0700

    rtl8712u: Add better build dependencies for Android

--- drivers/net/wireless/rtl8712u/Android.mk
+++ drivers/net/wireless/rtl8712u/Android.mk
@@ -10,16 +10,18 @@ LINUXPATH := kernel_imx
 MODULE_FILE := $(TARGET_OUT)/lib/modules/$(DRIVER).ko
 KERNELVER := $(shell cd $(LINUXPATH) && $(MAKE) ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- kernelrelease)
 
-$(MODULE_FILE):
+$(MODULE_FILE): $(DRIVER_SRC)/$(DRIVER).ko
+	$(ACP) $(DRIVER_SRC)/$(DRIVER).ko $(TARGET_OUT)/lib/modules/
+
+$(DRIVER_SRC)/$(DRIVER).ko:
 	@echo "Kernel version is $(KERNELVER)"
-	$(MAKE) ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- \
+	$(MAKE) V=1 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- \
 		TOPDIR=$(DRIVER_SRC) \
 		KVER=$(KERNELVER) \
 		-C $(LINUXPATH) \
 		M=$(DRIVER_SRC) \
 		modules
 	mkdir -p $(TARGET_OUT)/lib/modules/
-	$(ACP) $(DRIVER_SRC)/$(DRIVER).ko $(TARGET_OUT)/lib/modules/
 ALL_PREBUILT+=$(MODULE_FILE)
 
 endif
