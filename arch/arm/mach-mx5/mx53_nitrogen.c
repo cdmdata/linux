e67028a8b19d3fe0f66374473e3c2044b53cd818
Troy Kisky <troy.kisky@boundarydevices.com>
2011-05-11 12:44:15 -0700

    mx53_nitrogen: add pwm1, gpio_9 backlight

--- arch/arm/mach-mx5/mx53_nitrogen.c
+++ arch/arm/mach-mx5/mx53_nitrogen.c
@@ -415,7 +415,8 @@ static struct pad_desc mx53evk_pads[] = {
 	MX53_PAD_EIM_DA12__GPIO_3_12,
 
 	/* PWM backlight */
-	MX53_PAD_GPIO_1__PWMO,
+	MX53_PAD_GPIO_9__PWMO,		/* pwm1 */
+	MX53_PAD_GPIO_1__PWMO,		/* pwm2 */
 
 	/* USB HOST USB_PWR */
 	MX53_PAD_ATA_DA_2__GPIO_7_8,
@@ -640,8 +641,17 @@ static struct mxc_w1_config mxc_w1_data = {
 	.search_rom_accelerator = 1,
 };
 
-static struct platform_pwm_backlight_data mxc_pwm_backlight_data = {
+/* GPIO_1 lcd backlight(pwm2) */
+static struct platform_pwm_backlight_data mxc_backlight_data1 = {
 	.pwm_id = 1,
+	.max_brightness = 255,
+	.dft_brightness = 128,
+	.pwm_period_ns = 50000,
+};
+
+/* GPIO_9 backlight (pwm1) */
+static struct platform_pwm_backlight_data mxc_backlight_data2 = {
+	.pwm_id = 0,
 	.max_brightness = 255,
 	.dft_brightness = 128,
 	.pwm_period_ns = 50000,
@@ -1576,8 +1586,10 @@ static void __init mxc_board_init(struct i2c_board_info *bi0, int bi0_size,
 	*/
 
 	mxc_register_device(&mxc_iim_device, &iim_data);
+	mxc_register_device(&mxc_pwm1_device, NULL);
 	mxc_register_device(&mxc_pwm2_device, NULL);
-	mxc_register_device(&mxc_pwm1_backlight_device,	&mxc_pwm_backlight_data);
+	mxc_register_device(&mxc_pwm1_backlight_device,	&mxc_backlight_data1);
+	mxc_register_device(&mxc_pwm2_backlight_device,	&mxc_backlight_data2);
 
 
 /*	mxc_register_device(&mxc_keypad_device, &keypad_plat_data); */
