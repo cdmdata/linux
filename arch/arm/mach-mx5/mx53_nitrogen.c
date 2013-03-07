09a859181e799436881ac3fe03538186c756cb22
Troy Kisky <troy.kisky@boundarydevices.com>
2013-03-06 20:06:10 -0700

    nitrogen53a: add i2c bus changes
    These changes are not relevant to CDM
    but make comparing the file with boundary
    devices version easier.

--- arch/arm/mach-mx5/mx53_nitrogen.c
+++ arch/arm/mach-mx5/mx53_nitrogen.c
@@ -2049,15 +2049,29 @@ static struct i2c_board_info n53a_i2c5_board_info[] __initdata = {
 	},
 };
 
-/* EDID is here too */
 static struct i2c_board_info n53a_i2c6_board_info[] __initdata = {
+	{
+	 .type = "isl1208",
+	 .addr = 0x6f,
+	},
+};
+
+/* EDID is here too */
+static struct i2c_board_info n53a_i2c7_board_info[] __initdata = {
 	{
 	 .type = "bq20z75",		/* Battery */
 	 .addr = 0x0b,
 	},
 };
 
-static struct i2c_board_info n53a_i2c7_board_info[] __initdata = {
+static struct i2c_board_info n53a_i2c8_board_info[] __initdata = {
+	{
+	 .type = "bq20z75",		/* 2nd Battery */
+	 .addr = 0x0b,
+	},
+};
+
+static struct i2c_board_info n53a_i2c9_board_info[] __initdata = {
 	{
 	 .type = "sc16is7xx-uart",
 	 .addr = 0x49,
@@ -2068,17 +2082,20 @@ static struct i2c_board_info n53a_i2c7_board_info[] __initdata = {
 #endif
 
 #ifdef CONFIG_MACH_NITROGEN_A_IMX53
-/* i2c3-i2c6, Middle i2C bus has a switch */
+/* i2c3-i2c8, Middle i2C bus has a switch */
 static const unsigned n53a_i2c2_gpiomux_gpios[] = {
-	/* i2c3- i2c6*/
-	MAKE_GP(3, 7),		/* EIM_DA7 - PIC16F616_TOUCH */
-	MAKE_GP(3, 10),		/* EIM_DA10 - Camera */
-	MAKE_GP(3, 11),		/* EIM_DA11 - TFP410_ACCEL*/
-	MAKE_GP(6, 11)		/* NANDF_CS0 - BATT_EDID */
+	/* i2c3- i2c8*/
+	MAKE_GP(3, 6),		/* 1 - EIM_DA6 - battery select */
+	MAKE_GP(3, 7),		/* 2 - EIM_DA7 - PIC16F616_TOUCH */
+	MAKE_GP(3, 10),		/* 4 - EIM_DA10 - Camera */
+	MAKE_GP(3, 11),		/* 8 - EIM_DA11 - TFP410_ACCEL*/
+	MAKE_GP(6, 11),		/* 16 - NANDF_CS0 - BATT_EDID */
+	MAKE_GP(6, 12),		/* 32 - NANDF_WE - rtc isl1208 */
 };
 
 static const unsigned n53a_i2c2_gpiomux_values[] = {
-	1, 2, 4, 8
+/* bus	3  4  5   6   7   8 */
+	2, 4, 8, 32, 16, 16 | 1,
 };
 
 static struct gpio_i2cmux_platform_data n53a_i2c2_i2cmux_data = {
@@ -2099,9 +2116,9 @@ static struct platform_device n53a_i2c2_i2cmux = {
         },
 };
 
-/* i2c7, Last i2C bus has a buffer for UART */
+/* i2c9, Last i2C bus has a buffer for UART */
 static const unsigned n53a_i2c3_gpiomux_gpios[] = {
-	/* i2c7*/
+	/* i2c9*/
 	MAKE_GP(6, 10)		/* NANDF_RB0 - SC16IS7XX */
 };
 
@@ -2111,7 +2128,7 @@ static const unsigned n53a_i2c3_gpiomux_values[] = {
 
 static struct gpio_i2cmux_platform_data n53a_i2c3_i2cmux_data = {
 	.parent		= 2,
-	.base_nr	= 7, /* optional */
+	.base_nr	= 9, /* optional */
 	.values		= n53a_i2c3_gpiomux_values,
 	.n_values	= ARRAY_SIZE(n53a_i2c3_gpiomux_values),
 	.gpios		= n53a_i2c3_gpiomux_gpios,
@@ -2225,6 +2242,8 @@ static void __init mxc_board_init_nitrogen_a(void)
 	ldo8_consumers[0].dev_name = n53a_camera_i2c_dev_name;
 	ldo9_consumers[0].dev_name = n53a_camera_i2c_dev_name;
 	mxci2c2_data.i2c_clock_toggle = n53a_i2c_clock_toggle2;
+	/* eMMC settings */
+	mmc3_data.caps = MMC_CAP_MMC_HIGHSPEED | MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA;	// MMC_CAP_DATA_DDR causes errors
 
 #ifdef HAVE_CAMERA
 	camera_data.power_down = MAKE_GP(2, 22);
@@ -2258,9 +2277,13 @@ static void __init mxc_board_init_nitrogen_a(void)
 	i2c_register_board_info(4, n53a_i2c4_board_info, ARRAY_SIZE(n53a_i2c4_board_info));
 	i2c_register_board_info(5, n53a_i2c5_board_info, ARRAY_SIZE(n53a_i2c5_board_info));
 	i2c_register_board_info(6, n53a_i2c6_board_info, ARRAY_SIZE(n53a_i2c6_board_info));
+	i2c_register_board_info(7, n53a_i2c7_board_info, ARRAY_SIZE(n53a_i2c7_board_info));
+	i2c_register_board_info(8, n53a_i2c8_board_info, ARRAY_SIZE(n53a_i2c8_board_info));
 
 	mxc_register_device(&n53a_i2c3_i2cmux, &n53a_i2c3_i2cmux_data);
-	i2c_register_board_info(7, n53a_i2c7_board_info, ARRAY_SIZE(n53a_i2c7_board_info));
+	i2c_register_board_info(9, n53a_i2c9_board_info, ARRAY_SIZE(n53a_i2c9_board_info));
+
+	mxc_register_device(&mxc_bt_rfkill, &mxc_bt_rfkill_data);
 }
 
 MACHINE_START(NITROGEN_A_IMX53, "Boundary Devices Nitrogen_A MX53 Board")
@@ -2273,8 +2296,9 @@ MACHINE_END
 #endif
 
 /*****************************************************************************/
-extern struct platform_device mxc_uart_device3;
+extern struct platform_device mxc_uart_device1;
 extern struct platform_device mxc_uart_device2;
+extern struct platform_device mxc_uart_device3;
 static struct imxuart_platform_data uart_pdata = {
 	.flags = IMXUART_HAVE_RTSCTS,
 };
